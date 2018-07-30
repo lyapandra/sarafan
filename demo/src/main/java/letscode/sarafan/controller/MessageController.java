f5a637d5a0c9f55245cd174ccbeda9b1c3cf0073
lyapandra <lyapandra.andriy.s@gmail.com>
2018-07-30 19:56:50 +0300

    show List<Map<String,String>> messages

--- demo/src/main/java/letscode/sarafan/controller/MessageController.java
+++ demo/src/main/java/letscode/sarafan/controller/MessageController.java
@@ -4,6 +4,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by a on 7/30/2018.
  */
@@ -11,8 +16,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    @GetMapping
+    public List<Map<String,String>> messages = new ArrayList<Map<String,String>>(){{
+        add(new HashMap<String,String>() {{put("id","1");put("text","message#1");}});
+        add(new HashMap<String,String>() {{put("id","2");put("text","message#2");}});
+        add(new HashMap<String,String>() {{put("id","3");put("text","message#3");}});
+    }};
+
+    /*@GetMapping
     public String list(){
         return "index";
+    }*/
+
+    @GetMapping
+    public List<Map<String,String>>  list(){
+        return messages;
     }
+
+
 }
