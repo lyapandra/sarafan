5454bd18426ba60cb503e914a1fca710419fe204
lyapandra <lyapandra.andriy.s@gmail.com>
2018-08-01 23:01:01 +0300

    end of Spring Boot REST part 1 (-)

--- demo/src/main/resources/static/js/main.js
+++ demo/src/main/resources/static/js/main.js
@@ -1,6 +1,9 @@
 /**
  * Created by a on 8/1/2018.
  */
+
+var messageApi = Vue.resource('/message{/id}');
+
 Vue.component('message-row', {
     props: ['message'],
     template: '<div><i>({{message.id}})</i> {{message.text}} </div>'
@@ -8,18 +11,21 @@ Vue.component('message-row', {
 
 Vue.component('messages-list', {
     props: ['messages'],
-    template: '<div><message-row v-for="message in messages" :message="message" /></div>'
+    template: '<div><message-row v-for="message in messages" :key="message.id" :message="message" /></div>',
+    created: function() {
+        messageApi.get().then(result =>
+        result.json().then(data =>
+        data.forEach(message => this.messages.push(message))
+        )
+        )
+    }
 });
 
 var app = new Vue({
     el: '#app',
     template: '<messages-list :messages="messages"/>',
     data: {
-        messages: [
-            {id: '1', text: '001'},
-            {id: '2', text: '002'},
-            {id: '3', text: '003'},
-        ]
+        messages: []
     }
 });
 
